8551d95a22cc7215f087d5241a55a6abbb6dce34
Alex Osipenko <alexeyvxa@gmail.com>
2023-01-16 18:03:31 +0300

    update --help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
 import { Command } from 'commander';
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>');
+
+program.option('-f, --format <type>', 'output format');
 
 program.parse();
