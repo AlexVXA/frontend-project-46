584f94ba575f8b23b4981fcd34e8a0ed65c3496f
Alex Osipenko <alexeyvxa@gmail.com>
2023-01-27 14:34:52 +0300

    Write action command for compare two flat JSONs

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import compare from '../src/comparer.js';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>');
-
-program.option('-f, --format <type>', 'output format');
-
-program.parse();
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(compare(filepath1, filepath2));
+  })
+  .parse();
