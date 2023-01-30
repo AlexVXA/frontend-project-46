a0631f042e3949958f4146a1addb469d6a1d70ee
Alex Osipenko <alexeyvxa@gmail.com>
2023-01-30 19:51:56 +0300

    Make test for comparer.js

---
+++ __tests__/comparer.test.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@jest/globals';
+import { readFileSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/comparer.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fixturePath = (filename) => join(__dirname, '..', '__tests__', '__fixtures__', filename);
+const getFixture = (filename) => readFileSync(fixturePath(filename), 'utf8');
+test('stylish flat JSON', () => {
+  const expected = getFixture('stylish_flat.txt').trim();
+  const actual = genDiff(fixturePath('example1.json'), fixturePath('example2.json'));
+  expect(actual).toBe(expected);
+});
