eb4542d9885a4b78478f12061bf5c691bbaa50c5
Alex Osipenko <alexeyvxa@gmail.com>
2023-02-27 20:58:02 +0300

    Set stylish as default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import compare from '../src/index.js';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -8,8 +8,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(compare(filepath1, filepath2));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   })
   .parse();
