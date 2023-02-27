4db5af74af4c0e4c5d71fa35197bc2ce7e1ed585
Alex Osipenko <alexeyvxa@gmail.com>
2023-02-27 20:29:36 +0300

    Complete stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,31 +1,64 @@
+import isObj from '../isObj.js';
+
 /* eslint-disable object-curly-newline */
-/* eslint-disable no-param-reassign */
-const stylish = (arr) => {
-  const str = arr.reduce((acc, prop) => {
-    const { key, status, value, previous, current } = prop;
-    switch (status) {
-      case 'removed':
-        acc += `\n  - ${key}: ${value}`;
-        break;
-      case 'added':
-        acc += `\n  + ${key}: ${value}`;
-        break;
-      case 'updated':
-        acc += `\n  - ${key}: ${previous}\n  + ${key}: ${current}`;
-        break;
-      case 'unmodified':
-        acc += `\n    ${key}: ${value}`;
-        break;
-      case 'nested':
-        acc += String(stylish(prop.children));
-        break;
-      default:
-        acc += `\n   ${key}: ${value}`;
-        break;
-    }
-    return acc;
-  }, '');
-  return `{${str}\n}`;
+
+const getSpaces = (depth) => '    '.repeat(depth);
+
+const marks = {
+  openBracket: '{',
+  closeBracket(depth) {
+    return `${getSpaces(depth - 1)}}`;
+  },
+  removed: '- ',
+  added: '+ ',
+  unmodified: '  ',
+  nested: '  ',
+};
+
+const stringify = (node, depth = 1) => {
+  const indent = getSpaces(depth);
+  if (!isObj(node)) {
+    return `${node}`;
+  }
+  const strings = Object.entries(node).map(
+    ([key, val]) => `${indent}${key}: ${stringify(val, depth + 1)}`,
+  );
+  return [marks.openBracket, ...strings, marks.closeBracket(depth)].join('\n');
+};
+
+const stylish = (diffTree) => {
+  const iter = (node, depth) => {
+    const indent = getSpaces(depth).slice(2);
+    const strings = node.reduce((acc, prop) => {
+      const { key, status, value, previous, current, children } = prop;
+      const stringStarter = `${indent}${marks[status]}${key}`;
+      switch (status) {
+        case 'removed':
+          acc.push(`${stringStarter}: ${stringify(value, depth + 1)}`);
+          break;
+        case 'added':
+          acc.push(`${stringStarter}: ${stringify(value, depth + 1)}`);
+          break;
+        case 'updated':
+          acc.push(
+            `${indent}${marks.removed}${key}: ${stringify(previous, depth + 1)}\n${indent}${
+              marks.added
+            }${key}: ${stringify(current, depth + 1)}`,
+          );
+          break;
+        case 'nested':
+          acc.push(`${stringStarter}: ${iter(children, depth + 1)}`);
+          break;
+        case 'unmodified':
+        default:
+          acc.push(`${stringStarter}: ${stringify(value, depth + 1)}`);
+          break;
+      }
+      return acc;
+    }, []);
+    return [marks.openBracket, ...strings, marks.closeBracket(depth)].join('\n');
+  };
+  return iter(diffTree, 1);
 };
 
 export default stylish;
