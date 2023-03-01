02a66744bc41c57fe0a61ed67c1002c28cd13e22
Alex Osipenko <alexeyvxa@gmail.com>
2023-03-01 21:10:51 +0300

    Add plain ffixture and test, update args

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,26 +9,43 @@ const fixturePath = (filename) => join(__dirname, '..', '__tests__', '__fixtures
 const getFixture = (filename) => readFileSync(fixturePath(filename), 'utf8');
 test('stylish flat JSON', () => {
   const expected = getFixture('stylish_flat.txt').trim();
-  const actual = genDiff(fixturePath('example1.json'), fixturePath('example2.json'));
+  const actual = genDiff(
+    fixturePath('example1.json'),
+    fixturePath('example2.json'),
+    'stylish',
+  );
   expect(actual).toBe(expected);
 });
 test('stylish flat YAML', () => {
   const expected = getFixture('stylish_flat.txt').trim();
-  const actual = genDiff(fixturePath('example5.yml'), fixturePath('example6.yml'));
+  const actual = genDiff(fixturePath('example5.yml'), fixturePath('example6.yml'), 'stylish');
   expect(actual).toBe(expected);
 });
 test('stylish recursive JSON', () => {
   const expected = getFixture('stylish_recursive.txt').trim();
-  const actual = genDiff(fixturePath('example7.json'), fixturePath('example8.json'));
+  const actual = genDiff(
+    fixturePath('example7.json'),
+    fixturePath('example8.json'),
+    'stylish',
+  );
   expect(actual).toBe(expected);
 });
 test('stylish recursive YML', () => {
   const expected = getFixture('stylish_recursive.txt').trim();
-  const actual = genDiff(fixturePath('example9.yml'), fixturePath('example10.yml'));
+  const actual = genDiff(fixturePath('example9.yml'), fixturePath('example10.yml'), 'stylish');
   expect(actual).toBe(expected);
 });
 test('stylish recursive mixed', () => {
   const expected = getFixture('stylish_recursive.txt').trim();
-  const actual = genDiff(fixturePath('example7.json'), fixturePath('example10.yml'));
+  const actual = genDiff(
+    fixturePath('example7.json'),
+    fixturePath('example10.yml'),
+    'stylish',
+  );
+  expect(actual).toBe(expected);
+});
+test('plain recursive mixed', () => {
+  const expected = getFixture('plain.txt').trim();
+  const actual = genDiff(fixturePath('example7.json'), fixturePath('example10.yml'), 'plain');
   expect(actual).toBe(expected);
 });
